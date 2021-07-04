08fbfc1a7dc2647cd235b5506bc0f35c01bec777
Mateusz Rokosa <mtrokosa97@gmail.com>
2021-07-04 22:03:50 +0200

    EDR

--- main.py
+++ main.py
@@ -72,8 +72,12 @@ good_code.to_csv('result_good.csv', index=False)
 bad_code = pd.read_csv('result_bad.csv')
 good_code = pd.read_csv('result_good.csv')
 
-print(bad_code.groupby('Label')['TAR'].median())
-print(good_code.groupby('Label')['TAR'].median())
+# print(bad_code.groupby('Label')['TAR'].median())
+# print(good_code.groupby('Label')['TAR'].median())
+# print(bad_code.groupby('Label')['TAR'].mean())
+# print(good_code.groupby('Label')['TAR'].mean())
+# print(bad_code.groupby('Label')['TAR'].std())
+# print(good_code.groupby('Label')['TAR'].std())
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad1', 'TAR']))
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad1', 'TAR']))
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad2', 'TAR']))
@@ -86,17 +90,21 @@ print(good_code.groupby('Label')['TAR'].median())
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad3.3', 'TAR']))
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad3.4', 'TAR']))
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad3.4', 'TAR']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad1', 'TAR'], good_code.loc[bad_code['Label'] == 'zad1', 'TAR']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad2', 'TAR'], good_code.loc[bad_code['Label'] == 'zad2', 'TAR']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.1', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.1', 'TAR']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.2', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.2', 'TAR']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.3', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.3', 'TAR']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.4', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.4', 'TAR']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad1', 'TAR'], good_code.loc[bad_code['Label'] == 'zad1', 'TAR']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad2', 'TAR'], good_code.loc[bad_code['Label'] == 'zad2', 'TAR']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.1', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.1', 'TAR']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.2', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.2', 'TAR']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.3', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.3', 'TAR']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.4', 'TAR'], good_code.loc[bad_code['Label'] == 'zad3.4', 'TAR']))
 
 
 # Theta Band Power
-print(bad_code.groupby('Label')['Mean Theta'].median())
-print(good_code.groupby('Label')['Mean Theta'].median())
+# print(bad_code.groupby('Label')['Mean Theta'].median())
+# print(good_code.groupby('Label')['Mean Theta'].median())
+# print(bad_code.groupby('Label')['Mean Theta'].mean())
+# print(good_code.groupby('Label')['Mean Theta'].mean())
+# print(bad_code.groupby('Label')['Mean Theta'].std())
+# print(good_code.groupby('Label')['Mean Theta'].std())
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad1', 'Mean Theta']))
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad1', 'Mean Theta']))
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad2', 'Mean Theta']))
@@ -109,19 +117,23 @@ print(good_code.groupby('Label')['Mean Theta'].median())
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Theta']))
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Theta']))
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Theta']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad1', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad1', 'Mean Theta']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad2', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad2', 'Mean Theta']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Theta']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Theta']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Theta']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Theta']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad1', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad1', 'Mean Theta']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad2', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad2', 'Mean Theta']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Theta']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Theta']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Theta']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Theta'], good_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Theta']))
 
 
 # Alpha Band Power
-print(bad_code.groupby('Label')['Mean Alpha'].median())
-print(good_code.groupby('Label')['Mean Alpha'].median())
-# print(shapiro(bad_code.loc[bad_code['Label'] == 'zad1', 'Mean Alpha']))
-# print(shapiro(good_code.loc[bad_code['Label'] == 'zad1', 'Mean Alpha']))
+# print(bad_code.groupby('Label')['Mean Alpha'].median())
+# print(good_code.groupby('Label')['Mean Alpha'].median())
+print(bad_code.groupby('Label')['Mean Alpha'].mean())
+print(good_code.groupby('Label')['Mean Alpha'].mean())
+print(bad_code.groupby('Label')['Mean Alpha'].std())
+print(good_code.groupby('Label')['Mean Alpha'].std())
+print(shapiro(bad_code.loc[bad_code['Label'] == 'zad1', 'Mean Alpha']))
+print(shapiro(good_code.loc[bad_code['Label'] == 'zad1', 'Mean Alpha']))
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad2', 'Mean Alpha']))
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad2', 'Mean Alpha']))
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Alpha']))
@@ -133,11 +145,11 @@ print(good_code.groupby('Label')['Mean Alpha'].median())
 # print(shapiro(bad_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Alpha']))
 # print(shapiro(good_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Alpha']))
 print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad1', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad1', 'Mean Alpha']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad2', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad2', 'Mean Alpha']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Alpha']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Alpha']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Alpha']))
-print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Alpha']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad2', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad2', 'Mean Alpha']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.1', 'Mean Alpha']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.2', 'Mean Alpha']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Alpha']))
+# print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Alpha'], good_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Alpha']))
 
 
 # Beta Band Power
@@ -163,10 +175,57 @@ print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.3', 'Mean Beta'], goo
 print(mannwhitneyu(bad_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Beta'], good_code.loc[bad_code['Label'] == 'zad3.4', 'Mean Beta']))
 
 
+# ERD Alpha
+bad_code_pivot = pd.pivot_table(bad_code, values=['Mean Alpha', 'Mean Beta', "Mean Theta"], index='Respondent Name', columns='Label').reset_index()
+good_code_pivot = pd.pivot_table(good_code, values=['Mean Alpha', 'Mean Beta', "Mean Theta"], index='Respondent Name', columns='Label').reset_index()
 
 
+bad_code_pivot[('ERD zad1', 'ERD zad1')] = ((bad_code_pivot[('Mean Alpha', 'baseline-1')] - bad_code_pivot[('Mean Alpha', 'zad1')]) / bad_code_pivot[('Mean Alpha', 'baseline-1')]) * 100
+good_code_pivot[('ERD zad1', 'ERD zad1')] = ((good_code_pivot[('Mean Alpha', 'baseline-1')] - good_code_pivot[('Mean Alpha', 'zad1')]) / good_code_pivot[('Mean Alpha', 'baseline-1')]) * 100
+bad_code_pivot[('ERD zad2', 'ERD zad2')] = ((bad_code_pivot[('Mean Alpha', 'baseline-2')] - bad_code_pivot[('Mean Alpha', 'zad2')]) / bad_code_pivot[('Mean Alpha', 'baseline-2')]) * 100
+good_code_pivot[('ERD zad2', 'ERD zad2')] = ((good_code_pivot[('Mean Alpha', 'baseline-2')] - good_code_pivot[('Mean Alpha', 'zad2')]) / good_code_pivot[('Mean Alpha', 'baseline-2')]) * 100
+bad_code_pivot[('ERD zad3.1', 'ERD zad3.1')] = ((bad_code_pivot[('Mean Alpha', 'baseline-3')] - bad_code_pivot[('Mean Alpha', 'zad3.1')]) / bad_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
+good_code_pivot[('ERD zad3.1', 'ERD zad3.1')] = ((good_code_pivot[('Mean Alpha', 'baseline-3')] - good_code_pivot[('Mean Alpha', 'zad3.1')]) / good_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
+bad_code_pivot[('ERD zad3.2', 'ERD zad3.2')] = ((bad_code_pivot[('Mean Alpha', 'baseline-3')] - bad_code_pivot[('Mean Alpha', 'zad3.2')]) / bad_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
+good_code_pivot[('ERD zad3.2', 'ERD zad3.2')] = ((good_code_pivot[('Mean Alpha', 'baseline-3')] - good_code_pivot[('Mean Alpha', 'zad3.2')]) / good_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
+bad_code_pivot[('ERD zad3.3', 'ERD zad3.3')] = ((bad_code_pivot[('Mean Alpha', 'baseline-3')] - bad_code_pivot[('Mean Alpha', 'zad3.3')]) / bad_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
+good_code_pivot[('ERD zad3.3', 'ERD zad3.3')] = ((good_code_pivot[('Mean Alpha', 'baseline-3')] - good_code_pivot[('Mean Alpha', 'zad3.3')]) / good_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
+bad_code_pivot[('ERD zad3.4', 'ERD zad3.4')] = ((bad_code_pivot[('Mean Alpha', 'baseline-3')] - bad_code_pivot[('Mean Alpha', 'zad3.4')]) / bad_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
+good_code_pivot[('ERD zad3.4', 'ERD zad3.4')] = ((good_code_pivot[('Mean Alpha', 'baseline-3')] - good_code_pivot[('Mean Alpha', 'zad3.4')]) / good_code_pivot[('Mean Alpha', 'baseline-3')]) * 100
 
 
+print(bad_code_pivot[('ERD zad1', 'ERD zad1')].median())
+print(good_code_pivot[('ERD zad1', 'ERD zad1')].median())
+print(bad_code_pivot[('ERD zad2', 'ERD zad2')].median())
+print(good_code_pivot[('ERD zad2', 'ERD zad2')].median())
+print(bad_code_pivot[('ERD zad3.1', 'ERD zad3.1')].median())
+print(good_code_pivot[('ERD zad3.1', 'ERD zad3.1')].median())
+print(bad_code_pivot[('ERD zad3.2', 'ERD zad3.2')].median())
+print(good_code_pivot[('ERD zad3.2', 'ERD zad3.2')].median())
+print(bad_code_pivot[('ERD zad3.3', 'ERD zad3.3')].median())
+print(good_code_pivot[('ERD zad3.3', 'ERD zad3.3')].median())
+print(bad_code_pivot[('ERD zad3.4', 'ERD zad3.4')].median())
+print(good_code_pivot[('ERD zad3.4', 'ERD zad3.4')].median())
+print(shapiro(bad_code_pivot[('ERD zad1', 'ERD zad1')]))
+print(shapiro(good_code_pivot[('ERD zad1', 'ERD zad1')]))
+print(shapiro(bad_code_pivot[('ERD zad2', 'ERD zad2')]))
+print(shapiro(good_code_pivot[('ERD zad2', 'ERD zad2')]))
+print(shapiro(bad_code_pivot[('ERD zad3.1', 'ERD zad3.1')]))
+print(shapiro(good_code_pivot[('ERD zad3.1', 'ERD zad3.1')]))
+print(shapiro(bad_code_pivot[('ERD zad3.2', 'ERD zad3.2')]))
+print(shapiro(good_code_pivot[('ERD zad3.2', 'ERD zad3.2')]))
+print(shapiro(bad_code_pivot[('ERD zad3.3', 'ERD zad3.3')]))
+print(shapiro(good_code_pivot[('ERD zad3.3', 'ERD zad3.3')]))
+print(shapiro(bad_code_pivot[('ERD zad3.4', 'ERD zad3.4')]))
+print(shapiro(good_code_pivot[('ERD zad3.4', 'ERD zad3.4')]))
+print(mannwhitneyu(bad_code_pivot[('ERD zad1', 'ERD zad1')], good_code_pivot[('ERD zad1', 'ERD zad1')]))
+print(mannwhitneyu(bad_code_pivot[('ERD zad2', 'ERD zad2')].dropna(), good_code_pivot[('ERD zad2', 'ERD zad2')].dropna()))
+print(mannwhitneyu(bad_code_pivot[('ERD zad3.1', 'ERD zad3.1')].dropna(), good_code_pivot[('ERD zad3.1', 'ERD zad3.1')].dropna()))
+print(mannwhitneyu(bad_code_pivot[('ERD zad3.2', 'ERD zad3.2')].dropna(), good_code_pivot[('ERD zad3.2', 'ERD zad3.2')].dropna()))
+print(mannwhitneyu(good_code_pivot[('ERD zad3.3', 'ERD zad3.3')].dropna(), good_code_pivot[('ERD zad3.3', 'ERD zad3.3')].dropna()))
+print(mannwhitneyu(bad_code_pivot[('ERD zad3.4', 'ERD zad3.4')].dropna(), good_code_pivot[('ERD zad3.4', 'ERD zad3.4')].dropna()))
+
+bad_code_pivot.to_csv('bad_code_pivot.csv')
 
 
 
